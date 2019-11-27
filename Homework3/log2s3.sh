e4d0e9020bd02dfce127248d58df3b93d17990ca
dina89 <dstefansky@gmail.com>
2019-11-27 20:17:00 +0200

    updated log2s3.sh file with path to aws2, added crontab logging

--- Homework3/log2s3.sh
+++ Homework3/log2s3.sh
@@ -1,1 +1,2 @@
-aws2 s3 cp /var/log/nginx/access.log s3://opsschool-dina-state-storage-s3/temp/
+#! /bin/bash -xv
+/usr/local/bin/aws2 s3 cp /var/log/nginx/access.log s3://opsschool-dina-state-storage-s3/temp/
--- Homework3/modules/server-cluster/webservers.tf
+++ Homework3/modules/server-cluster/webservers.tf
@@ -41,7 +41,8 @@ resource "aws_instance" "public_server" {
           "curl 'https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
           "unzip awscliv2.zip",
           "sudo ./aws/install",
-          "sudo echo \"$(echo \"5 * * * * /tmp/log2s3.sh\" ; crontab -l)\" | crontab -"
+          "chmod +x /tmp/log2s3.sh",
+          "sudo echo \"$(echo \"30 * * * * /tmp/log2s3.sh >> /tmp/log2s3.log 2>&1 \" ; crontab -l)\" | crontab -"
       ]
   }
 
